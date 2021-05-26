7dbdc99fb844429ff7d803e571d0b5ba799a28f4
Pavel Kolomnikov <pekolomnikov@avito.ru>
2021-05-26 12:32:54 +0700

    some shader magic

--- LearnOpenGL/Chapter02/Chapter02/render.cpp
+++ LearnOpenGL/Chapter02/Chapter02/render.cpp
@@ -7,6 +7,7 @@
 #include <string>
 
 #include "render.hpp"
+#include "shader.hpp"
 
 // GLEW
 #define GLEW_STATIC
@@ -15,42 +16,73 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
-const float vertexPositions[] = {
-    0.75f, 0.75f, 0.0f, 1.0f,
-    0.75f, -0.75f, 0.0f, 1.0f,
-    -0.75f, -0.75f, 0.0f, 1.0f,
-};
 GLuint positionBufferObject;
+GLuint shaderProgram;
+GLuint vao;
 
+// triangle
+const float vertexPositions[] = {
+    -0.75f, -0.75f, 0.0f, 1.0f,
+     0.75f, -0.75f, 0.0f, 1.0f,
+     0.75f,  0.75f, 0.0f, 1.0f,
+    
+//    -0.1f, -0.1f, 0.0f, 1.0f,
+//     0.1f, -0.1f, 0.0f, 1.0f,
+//     0.0f,  0.1f, 0.0f, 1.0f,
+//
+//    -0.3f, -0.1f, 0.0f, 1.0f,
+//    -0.1f, -0.1f, 0.0f, 1.0f,
+//    -0.2f,  0.1f, 0.0f, 1.0f,
+};
 
-const std::string strFragmentShader(
-    "#version 330                                   \n"
-    "out vec4 outputColor;                          \n"
-    "void main()                                    \n"
-    "{                                              \n"
-    "   outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f); \n"
-    "}                                              \n"
-);
-
-void init()
+// INIT PROGRAMM
+void initializeVertexBuffer()
 {
     glGenBuffers(1, &positionBufferObject);
     
     glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
+void initializeShaders()
+{
+    std::vector<GLuint> shaders;
+    
+    shaders.push_back(loadShaderFromFile(GL_FRAGMENT_SHADER, "FragPosition.frag"));
+    shaders.push_back(loadShaderFromFile(GL_VERTEX_SHADER, "FragPosition.vert"));
+    
+    shaderProgram = createProgram(shaders);
+    
+    std::for_each(shaders.begin(), shaders.end(), glDeleteShader);
+}
+
+void init()
+{
+    initializeVertexBuffer();
+    initializeShaders();
+    
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+}
+
+// LOOP
 void display()
 {
     glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
     glClear( GL_COLOR_BUFFER_BIT );
     
+    glUseProgram(shaderProgram);
+    
     glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0 , 0);
+        
+    glDrawArrays(GL_TRIANGLES, 0, 9);
     
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDisableVertexAttribArray(0);
+    glUseProgram(0);
 }
 
 void reshape (int w, int h)
