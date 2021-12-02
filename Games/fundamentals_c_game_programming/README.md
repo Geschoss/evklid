1df47f8f3519678abbf4f3380984570d678e965c
Geschoss <q11254@gmail.com>
2021-12-02 19:06:28 +0300

    fgd | init

---
+++ Games/fundamentals_c_game_programming/README.md
@@ -0,0 +1,3 @@
+## fundamentals c game programming
+
+[notion](https://www.notion.so/fundamentals-c-game-programming-7e0fb0b3bed64cb1b30b05a5b900d600)
---
+++ Games/fundamentals_c_game_programming/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    std::string mystring;
+    std::cin >> mystring;
+    std::cout << "Hello " << mystring;
+
+    return 0;
+}
